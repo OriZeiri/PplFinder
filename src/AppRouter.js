c1cd79278e70c528ab3beaf526431c4c56e4d638
Ori Zeiri <oriz400@gmail.com>
2022-08-07 01:05:08 +0300

    Add to store and apply to navbar

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,14 +2,16 @@ import React from "react";
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from "react-redux";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "pages";
+import { Home, Favorites } from "pages";
 import { ThemeProvider } from "theme";
 import NavBar from "components/NavBar";
 import nationsReducer from "redux/reducers/nationsReducer";
+import favoritesReducers from "redux/reducers/favoritesReducers";
 
 const store = configureStore({
   reducer:{
     nations : nationsReducer,
+    favoritesUsers : favoritesReducers,
   }
 })
 
@@ -21,6 +23,7 @@ const AppRouter = () => {
           <NavBar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/favorites" component={Favorites} />
           </Switch>
         </Router>
       </Provider>
